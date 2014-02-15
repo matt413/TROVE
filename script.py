e7bf09fd6131ad89dffaba0db1055021c2cb806b
matt413 <sk849@cornell.edu>
2014-02-15 15:43:39 -0500

    Made some updates using sqlite3 - middle of progress

--- script.py
+++ script.py
@@ -13,11 +13,29 @@
 Eventually, we may add for [exam]
 
 Exam name (eg, CT abdomen)
-Exam report (free text report of findings)"""
+Exam report (free text report of findings)
+
+
+In addition to an application that's constantly running, 
+it would also be nice if we can set parameters for 
+your script to generate exams within a certain date interval, eg,
+
+Generate 1000 exams between 2014-01-01 and 2014-01-31
+
+CONSTRAINTS: - DOB < Exam DateTime < Exam Finalized DateTime
+- Usually Exam Finalized DateTime is within 1-6 hours of Exam DateTime."""
 
 from faker import Factory
 import random
+import sqlite3
 
+# Initialize the database
+conn = sqlite3.connect('TROVE.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE Patients
+             (Name text, DOB text, Gender text, MRN int)''')
+
+"""
 def createData():
 	faker = Factory.create()
 	patientName = faker.first_name() + ' ' + faker.last_name()
@@ -41,7 +59,6 @@ def createData():
 	else:
 		examMod = 'US'
 	examDateTime = str(faker.date_time())
-	print examDateTime.index('-')
 	while int(examDateTime[:examDateTime.index('-')])<int(patientDOB[:patientDOB.index('-')]):
 		examDateTime = str(faker.date_time())
 	examID = str(random.randint(0,99999999)).zfill(8)
@@ -53,4 +70,73 @@ def createData():
 	print 'Exam ID: ' + examID
 	print 'Exam report: ' + examReportDateTime
 
-createData()
+createData() """
+
+class Exam:
+	def __init(self, Patient)__:
+		faker = Factory.create()
+		self.examMod = ''
+		self.randExam = random.randint(1, 4)
+		if randExam is 1:
+			self.examMod = 'CT'
+		elif randExam is 2:
+			self.examMod = 'MR'
+		elif randExam is 3:
+			self.examMod = 'XR'
+		else:
+			self.examMod = 'US'
+		self.examDateTime = str(faker.date_time())
+		while int(self.examDateTime[:self.examDateTime.index('-')])<int(Patient.DOB()[:Patient.DOB().index('-')]):
+			self.examDateTime = str(faker.date_time())
+		self.examID = str(random.randint(0,99999999)).zfill(8)
+		self.examReportDateTime = str(faker.date_time())
+		while int(self.examReportDateTime[:self.examReportDateTime.index('-')])<int(self.examDateTime[:self.examDateTime.index('-')]):
+			self.examReportDateTime = str(faker.date_time())
+		
+	def toString(self):
+		return 'Exam Modality: '+self.examMod+'\nExam Date/Time: '+examDateTime+'\nExam ID: '+examID+'\nExam report: '+examReportDateTime
+
+	def Modality(self):
+		return self.examMod
+
+	def examDate(self):
+		return self.examDateTime
+
+	def examReportDate(self):
+		return self.examReportDateTime
+
+class Patient():
+	def __init(self)__:
+		faker = Factory.create()
+		self.patientName = faker.first_name() + ' ' + faker.last_name()
+		self.patientDOB = faker.date()
+		self.genderInt = random.randint(1, 10)
+		self.patientGender = 'M' if genderInt <= 5 else 'F'
+		self.randMRN = str(random.randint(0, 99999999))
+		self.MRN = randMRN.zfill(8)
+		self.patientExams = []
+		c = conn.cursor()
+		c.execute("INSERT INTO Patients VALUES (self.patienName, self.patientDOB, self.patientGender, self.MRN)")
+		conn.commit()
+		conn.close()
+
+	def toString(self):
+		return 'Name: '+self.patientName+'\nDOB: '+self.patientDOB+'\nGender: '+self.patientGender+'\nMRN: '+self.MRN
+
+	def Name(self):
+		return self.patientName
+
+	def DOB(self):
+		return self.patientDOB
+
+	def Gender(self):
+		return self.patientGender
+
+	def MRN(self):
+		return self.MRN
+
+
+
+c.execute("INSERT INTO Patients VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+conn.commit()
+conn.close()
